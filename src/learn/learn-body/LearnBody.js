b62520830481ebc8a9b88b5d056ffab8db642956
U <U@esras-MacBook-Pro.local>
2019-11-25 00:29:59 +0300

    add pages

--- src/learn/learn-body/LearnBody.js
+++ src/learn/learn-body/LearnBody.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import './LearnBody.css'
+import { Card, Row, Col, Accordion, Button, ListGroup } from 'react-bootstrap'
 
 export default function LearnBody(props) {
     return (
         <>
-            <div>test learn body</div>
+            <div className="Content">
+                <Card className="CardCenterWidth CourseOverview">
+                    <Card.Body>
+                        <Row>
+                            <Col xs={3}>
+                                logo
+                            </Col>
+
+                            <Col xs={9}>
+                                <Card.Title>
+                                    Python 3 Tutorial
+                            </Card.Title>
+
+                                <Card.Text>
+                                    <p>Learn Python in the most social and fun way, with SoloLearn!</p>
+                                    <p>Learn Python, one of today's most in-demand programming languages on-the-go, while playing, for FREE! Compete and collaborate with your fellow SoloLearners, while surfing through short lessons and fun quizzes. Practice writing Python code within the app, collect points, and show off your skills. </p>
+                                    <p>When you complete the course, you'll win a Certificate of Completion as a trophy!</p>
+                                    <p>So don’t wait; dive right in! Start coding with Python!</p>
+                                </Card.Text>
+                            </Col>
+                        </Row>
+                    </Card.Body>
+                </Card>
+            </div>
         </>
     )
 }
--- src/learn/learn-header/LearnHeader.js
+++ src/learn/learn-header/LearnHeader.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import './LearnHeader.css'
+import { Navbar } from 'react-bootstrap'
+
 
 export default function LearnHeader(props) {
     return (
         <>
-            <div>learn header test</div>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Text>
+                    What is Python?
+                </Navbar.Text>
+                <Navbar.Collapse className="justify-content-end">
+                    <Navbar.Text>
+                        Signed in as: <a href="#login">Mark Otto</a>
+                    </Navbar.Text>
+                </Navbar.Collapse>
+            </Navbar>
         </>
     )
 }
